66d2001ab9afa1d5a23544b9e5ebe808138df09d
Henrich Lacao <henrich.boom@gmail.com>
2024-03-05 01:30:25 +0800

    naming

--- resources/views/customer/customerprofile.blade.php
+++ resources/views/customer/customerprofile.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css','resources/js/app.js'])
-    <title>Admin Profile</title>
+    <title>Customer Profile</title>
     
 </head>
 <body class="bg-green-200 ">
